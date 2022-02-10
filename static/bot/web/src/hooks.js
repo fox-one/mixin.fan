d5c25ac119041431fe7cf3283fe8c1846e526c7b
lidian <mail@imlidian.com>
2022-02-10 16:45:28 +0800

    delete source code

--- static/bot/web/src/hooks.js
+++
@@ -1,8 +0,0 @@
-import {createContext, useContext} from 'react';
-
-export const UserContext = createContext(null);
-export const UserProvider = UserContext.Provider;
-
-export function useUser() {
-  return useContext(UserContext);
-}
